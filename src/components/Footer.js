d885c5aee1c0723fb2350c0ab104c0d1dc995cef
hibeeeekay <hibeeeekay@gmail.com>
2021-05-07 17:34:45 +0100

    update

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <p className='d-flex justify-content-center align-items-center fw-bold'>CREATED BY IBUKUN SAMUEL</p>
+    );
+};
+
+export default Footer;
